9f607814a055f6544c73b1aea3d6d4a88da3fe66
xeten <mediatharlam@gmail.com>
2026-02-08 20:47:12 +0545

    the change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  trailingSlash: 'never', // Add this line
 });
