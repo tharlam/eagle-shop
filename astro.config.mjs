8a55f48cd7d71f2958c441409bdaf0320604c1b2
xeten <mediatharlam@gmail.com>
2026-02-09 20:15:02 +0545

    the change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel'; // Removed /serverless
 
 export default defineConfig({
   output: 'server', // This enables Server-Side Rendering (SSR)
-  adapter: vercel(), // This connects your project to Vercel's serverless brain
+  adapter: vercel(), // This connects your project to Vercel's brain
 });
